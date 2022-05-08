f5185da78267c2a5e458e6393c222f2dd4f8b178
Jules Robichaud-Gagnon <j.robichaudg@gmail.com>
2022-05-08 18:10:42 -0400

    mqtt bindings

---
+++ mqtt2lirc.py
@@ -0,0 +1,36 @@
+import paho.mqtt.client as mqtt
+import json
+import argparse
+import traceback
+
+parser = argparse.ArgumentParser(description='mqtt2lirc')
+parser.add_argument("hostname", help="broker hostname")
+parser.add_argument('--port', dest="port", type=int, default=1883, help='broker port')
+parser.add_argument('--username', '-u', dest='username', help='broker username')
+parser.add_argument('--password', '-p', dest='password', help='broker password')
+args = parser.parse_args()
+
+client = mqtt.Client(client_id="lirc")
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe("lirc/tx")
+
+def on_message(client, userdata, message):
+    try:
+        print("message received " ,str(message.payload.decode("utf-8")))
+        payload = json.loads(message.payload)
+        remote = payload["remote"]
+        keys = payload["keys"]
+        print(remote, keys)
+    except Exception:
+        traceback.print_exc()
+
+client.on_message = on_message
+client.on_connect = on_connect
+
+if args.username or args.password:
+    client.username_pw_set(username=args.username, password=args.password)
+
+client.connect(args.hostname, port=args.port, keepalive=60, bind_address="")
+client.loop_forever()
